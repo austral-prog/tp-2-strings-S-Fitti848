4b2285f3094bf35baf5b1a0cb9549bbdf83dfc59
S-Fitti848 <santiagofitti@gmail.com>
2026-03-29 11:54:52 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,20 +1,25 @@
 def change():
     print("Ingresar gasto:")
     gasto_raw = input()
+    print(gasto_raw)
     gasto = float(gasto_raw)
 
     print("Dinero recibido")
     dinero_raw = input()
+    print(dinero_raw)
     dinero = int(dinero_raw)
 
     vuelto = dinero - gasto
+    vuelto_str = f"{vuelto:.2f}"
+    partes = vuelto_str.split(".")
 
     print("")
     print("Vuelto")
     print("")
     print("Pesos:")
-    print(int(vuelto))
+    print(int(partes[0]))
     print("Centavos:")
-    print(round((vuelto - int(vuelto)) * 100))
+    print(int(partes[1]))
 
-change()
+if __name__ == "__main__":
+    change()
